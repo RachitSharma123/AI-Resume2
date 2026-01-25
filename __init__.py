8cd45559c8c38b0a440565c4ee3c057a3a1efc60
Rachit Sharma <rachitsharma2671300@gmail.com>
2026-01-26 10:07:46 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
-from .pdf_generator import create_resume_pdf, create_cover_letter_pdf
-from .utils import safe_filename, open_pdf_in_new_tub
-from .drawing_utils import (
+from pdf_generator import create_resume_pdf, create_cover_letter_pdf
+from utils import safe_filename, open_pdf_in_new_tub
+from drawing_utils import (
     PAGE_W, PAGE_H, MIN_LINE_GAP,
     wrap_lines, draw_boxed_block, draw_wrapped_text,
     draw_section_title, draw_bullets, draw_divider,
