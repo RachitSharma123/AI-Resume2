db3458750fbeb7a98db498a9060138b56152a663
Rachit Sharma <rachitsharma2671300@gmail.com>
2026-01-26 10:31:27 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,20 @@
-# app/__init__.py
+# __init__.py
 from pdf_generator import create_resume_pdf, create_cover_letter_pdf
-from utils import safe_filename, open_pdf_in_new_tub
+from utils import safe_filename, open_pdf_in_new_tab
 from drawing_utils import (
     PAGE_W, PAGE_H, MIN_LINE_GAP,
     wrap_lines, draw_boxed_block, draw_wrapped_text,
     draw_section_title, draw_bullets, draw_divider,
     draw_page_border
+)
+from ai_functions import (
+    call_ai_tailor_resume,
+    call_ai_generate_cover_letter,
+    call_ai_ats_score,
+    call_ai_improve_bullets,
+    call_ai_extract_keywords,
+    call_ai_rewrite_objective,
+    call_ai_compress_resume
 )
 
 __all__ = [
@@ -16,5 +25,12 @@ __all__ = [
     'PAGE_W', 'PAGE_H', 'MIN_LINE_GAP',
     'wrap_lines', 'draw_boxed_block', 'draw_wrapped_text',
     'draw_section_title', 'draw_bullets', 'draw_divider',
-    'draw_page_border'
+    'draw_page_border',
+    'call_ai_tailor_resume',
+    'call_ai_generate_cover_letter',
+    'call_ai_ats_score',
+    'call_ai_improve_bullets',
+    'call_ai_extract_keywords',
+    'call_ai_rewrite_objective',
+    'call_ai_compress_resume'
 ]
