8745d2d65b0f1d926d9746f08b32a1ed63b12bf2
Rachit Sharma <rachitsharma2671300@gmail.com>
2026-01-26 13:00:01 +1100

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -150,7 +150,7 @@ def create_resume_pdf(json_path="resume_data.json",
     
     # Education
     y = draw_section_title(c, "EDUCATION", left, y, size=10)  # Reduced from 11
-    y -= .5
+    y += .5
                          # Reduced spacing
     
     col_gap = 0.6 * cm
